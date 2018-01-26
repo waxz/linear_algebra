295e87b336f68d929e0afad54a8d10dbfbeda89d
waxz <wz67766@163.com>
2018-01-26 21:11:59 +0800

    statiic function;template

--- main.cpp
+++ main.cpp
@@ -3,60 +3,113 @@
 #include <cassert>
 #include <cmath>
 using namespace std;
+
+template<class Typ>
 class Matrix
 {
 public:
     //construct
 
-    Matrix();
-    Matrix(const Matrix & m);
-    Matrix(vector<double>,int , int );
+    Matrix(const Matrix &r_v);
+
+    Matrix(vector<Typ>, int, int);
+
+    explicit Matrix(vector<vector<Typ> >);
+
     ~Matrix();
 
     //member value
 
     pair<int,int> shape;
+
     int size();
 
+    vector<vector<Typ> > vec;
+
     //function
-    Matrix reshape(int,int);
+    Matrix reshape(int, int);
+
     Matrix T();
+
+    //static function
+    static Matrix ones(int, int);
+
+    static Matrix zeros(int, int);
+
+    static Matrix ones_like(Matrix &r_v);
+
+    static Matrix zeros_like(Matrix &r_v);
+
+    static Matrix arange(double, double, double);
+
+    static Matrix linspace(double, double, int);
+
+    static Matrix logspace(double, double, int);
+
+    static Matrix concat(Matrix, Matrix);
+
+    static Matrix stack(Matrix, Matrix);
+
+    static Matrix where(Matrix);
+
 
     //operate
     Matrix operator -();
-    Matrix operator +();
-    Matrix operator -(const Matrix & right_value);
-    Matrix operator +(const Matrix & right_value);
-    Matrix operator -(double right_value);
-    Matrix operator +(double right_value);
-    Matrix operator *(double right_value);
-    Matrix operator /(double right_value);
-    Matrix operator *(const Matrix &right_value);
-    Matrix operator /(const Matrix &right_value);
 
-    vector<double> operator[](int idx);
+    Matrix operator +();
+
+    Matrix operator-(const Matrix &r_v);
+
+    Matrix operator+(const Matrix &r_v);
+
+    Matrix operator-(Typ r_v);
+
+    Matrix operator+(Typ r_v);
+
+    Matrix operator*(Typ r_v);
+
+    Matrix operator/(Typ r_v);
+
+    Matrix operator*(const Matrix &r_v);
+
+    Matrix operator/(const Matrix &r_v);
+
+    Matrix operator>(double &r_v);
+
+    Matrix operator<(double &r_v);
+
+
+    vector<Typ> operator[](int idx);
+
 
     //matrx compute
-    double cross(const Matrix &right_value);
-    double dot(const Matrix &right_value);
+    Typ cross(const Matrix &r_v);
+
+    Typ dot(const Matrix &r_v);
 
     //
     double sum();
+
     double max();
+
     double min();
+
     Matrix normal();
+
     double sum(int axis);
+
     double max(int axis);
+
     double min(int axis);
 private:
     void descontruc();
-    vector<vector<double> > vec;
 
 
 };
 
 //col vector,col num,row num
-Matrix::Matrix(vector<double> array, int col, int row) : vec(col,vector<double>(row,1)) ,shape(col,row){
+template<class Typ>
+Matrix<Typ>::Matrix(vector<Typ> array, int col, int row) : vec(col, vector<Typ>(row, (Typ) 1)), shape(col, row) {
     assert(col != 0 && row != 0);
     for (int i = 0; i < col; i++) {
         for (int j = 0; j < row; j++) {
@@ -65,27 +118,38 @@ Matrix::Matrix(vector<double> array, int col, int row) : vec(col,vector<double>(
     }
 }
 
-Matrix::Matrix(const Matrix & m) {
+template<class Typ>
+Matrix<Typ>::Matrix(vector<vector<Typ> > init_vec) {
+    vec = init_vec;
+    shape.first = init_vec.size();
+    shape.second = init_vec[0].size();
+}
+
+template<class Typ>
+Matrix<Typ>::Matrix(const Matrix &m) {
     this->shape = m.shape;
     this->vec = m.vec;
 }
 
-
-void Matrix::descontruc() {
+template<class Typ>
+void Matrix<Typ>::descontruc() {
     cout<<"destruction!!!";
-    vector<vector<double> > ().swap(vec);
+    vector<vector<Typ> >().swap(vec);
 }
 
-Matrix::~Matrix() {
+template<class Typ>
+Matrix<Typ>::~Matrix() {
     descontruc();
 
 }
 
-
-int Matrix::size() {
+template<class Typ>
+int Matrix<Typ>::size() {
     return shape.first*shape.second;
 }
-Matrix Matrix::reshape(int col, int row) {
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::reshape(int col, int row) {
     assert(row*col == this->size());
     Matrix res(*this);
 
@@ -104,7 +168,8 @@ Matrix Matrix::reshape(int col, int row) {
 
 }
 
-Matrix Matrix::T() {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::T() {
     Matrix res(*this);
 
     vector<vector<double> > new_vec(shape.second,vector<double>(shape.first,1));
@@ -121,8 +186,8 @@ Matrix Matrix::T() {
     return res;
 }
 
-//-
-Matrix  Matrix::operator-() {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator-() {
 
 
     Matrix res(*this);
@@ -135,7 +200,8 @@ Matrix  Matrix::operator-() {
 
 }
 
-Matrix  Matrix::operator+() {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator+() {
 
 
     Matrix res(*this);
@@ -148,94 +214,99 @@ Matrix  Matrix::operator+() {
 
 }
 
-Matrix Matrix::operator+(double right_value) {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator+(Typ r_v) {
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j] + right_value;
+            res.vec[i][j] = res.vec[i][j] + r_v;
         }
     }
     return res;
 }
 
-Matrix Matrix::operator-(double right_value) {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator-(Typ r_v) {
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j] - right_value;
+            res.vec[i][j] = res.vec[i][j] - r_v;
         }
     }
     return res;
 }
 
-Matrix Matrix::operator+(const Matrix & right_value) {
-    assert(right_value.shape == shape);
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator+(const Matrix &r_v) {
+    assert(r_v.shape == shape);
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j] + right_value.vec[i][j] ;
+            res.vec[i][j] = res.vec[i][j] + r_v.vec[i][j];
         }
     }
     return res;
 }
 
-Matrix Matrix::operator-(const Matrix &right_value) {
-    assert(right_value.shape == shape);
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator-(const Matrix &r_v) {
+    assert(r_v.shape == shape);
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j] - right_value.vec[i][j] ;
+            res.vec[i][j] = res.vec[i][j] - r_v.vec[i][j];
 
         }
     }
     return res;
 }
 
-Matrix Matrix::operator *(const Matrix &right_value)
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator*(const Matrix &r_v) {
+    assert(r_v.shape == shape);
+    Matrix res(*this);
+    for (int i = 0; i < res.shape.first; i++) {
+        for (int j = 0; j < res.shape.second; j++) {
+            res.vec[i][j] = res.vec[i][j] * r_v.vec[i][j];
+        }
+    }
+    return res;
+}
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator/(const Matrix &r_v) {
+    assert(r_v.shape == shape);
+    Matrix res(*this);
+    for (int i = 0; i < res.shape.first; i++) {
+        for (int j = 0; j < res.shape.second; j++) {
+            assert(r_v.vec[i][j] != 0);
+            res.vec[i][j] = res.vec[i][j] / r_v.vec[i][j];
+        }
+    }
+    return res;
+}
+
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator*(Typ r_v)
 {
-    assert(right_value.shape == shape);
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j] * right_value.vec[i][j] ;
+            res.vec[i][j] = res.vec[i][j] * r_v;
         }
     }
     return res;
 }
 
-Matrix Matrix::operator /(const Matrix &right_value)
-{
-    assert(right_value.shape == shape);
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::operator/(Typ r_v) {
     Matrix res(*this);
     for (int i = 0; i < res.shape.first; i++) {
         for (int j = 0; j < res.shape.second; j++) {
-            assert(right_value.vec[i][j] != 0);
-            res.vec[i][j] = res.vec[i][j]/ right_value.vec[i][j] ;
-        }
-    }
-    return res;
-}
+            assert(r_v != 0);
 
-
-
-Matrix Matrix::operator *(double right_value)
-{
-    Matrix res(*this);
-    for (int i = 0; i < res.shape.first; i++) {
-        for (int j = 0; j < res.shape.second; j++) {
-            res.vec[i][j] = res.vec[i][j]* right_value ;
-        }
-    }
-    return res;
-}
-
-Matrix Matrix::operator/(double right_value) {
-    Matrix res(*this);
-    for (int i = 0; i < res.shape.first; i++) {
-        for (int j = 0; j < res.shape.second; j++) {
-            assert(right_value != 0);
-
-            res.vec[i][j] = res.vec[i][j] /right_value ;
+            res.vec[i][j] = res.vec[i][j] / r_v;
 
         }
     }
@@ -243,7 +314,8 @@ Matrix Matrix::operator/(double right_value) {
 }
 
 //norma
-Matrix Matrix::normal() {
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::normal() {
     double vec_sum = sqrt(((*this)*(*this)).sum());
     Matrix res = *this/vec_sum;
     return res;
@@ -251,7 +323,8 @@ Matrix Matrix::normal() {
 }
 
 //sum
-double Matrix::sum() {
+template<class Typ>
+double Matrix<Typ>::sum() {
     double res=0;
     for (int i = 0; i < shape.first; i++) {
         for (int j = 0; j < shape.second; j++) {
@@ -261,7 +334,8 @@ double Matrix::sum() {
     return res;
 }
 
-double Matrix::min() {
+template<class Typ>
+double Matrix<Typ>::min() {
     double res=0;
     for (int i = 0; i < shape.first; i++) {
         for (int j = 0; j < shape.second; j++) {
@@ -271,7 +345,9 @@ double Matrix::min() {
     }
     return res;
 }
-double Matrix::max() {
+
+template<class Typ>
+double Matrix<Typ>::max() {
     double res=0;
     for (int i = 0; i < shape.first; i++) {
         for (int j = 0; j < shape.second; j++) {
@@ -283,28 +359,65 @@ double Matrix::max() {
 }
 
 //[]
-vector<double> Matrix::operator[](int idx){
+template<class Typ>
+vector<Typ> Matrix<Typ>::operator[](int idx) {
     return vec[idx];
 }
 //cross sum = x1*y2 - y1*x2
-double Matrix::cross(const Matrix &right_value)
-{
-    assert(right_value.shape.first == 1&&shape.first == 1 &&right_value.shape.second == 2 && shape.second == 2);
-    double cross_sum = vec[0][0]*right_value.vec[0][1] -  vec[0][1]*right_value.vec[0][0];
+template<class Typ>
+Typ Matrix<Typ>::cross(const Matrix &r_v) {
+    assert(r_v.shape.first == 1 && shape.first == 1 && r_v.shape.second == 2 && shape.second == 2);
+    double cross_sum = vec[0][0] * r_v.vec[0][1] - vec[0][1] * r_v.vec[0][0];
     return cross_sum;
 
 
 }
 //dot sum =x1*x2 + y1*y2 + z1*z2
-double Matrix::dot(const Matrix &right_value)
-{
-    assert(right_value.shape.first == 1 && shape.first == 1);
-    double dot_sum = (*this*right_value).sum();
+template<class Typ>
+Typ Matrix<Typ>::dot(const Matrix &r_v) {
+    assert(r_v.shape.first == 1 && shape.first == 1);
+    double dot_sum = (*this * r_v).sum();
     return dot_sum;
 
 }
 
 
+//static function
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::ones(int col, int row) {
+    vector<vector<Typ> > new_vec(col, vector<Typ>(row, (Typ) 1));
+    Matrix<Typ> res(new_vec);
+    return res;
+
+
+}
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::zeros(int col, int row) {
+    vector<vector<Typ> > new_vec(col, vector<Typ>(row, (Typ) 0));
+    Matrix<Typ> res(new_vec);
+    return res;
+
+
+}
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::ones_like(Matrix &r_v) {
+    cout << "ones_like";
+    vector<vector<Typ> > new_vec(r_v.shape.first, vector<Typ>(r_v.shape.second, (Typ) 1));
+    Matrix<Typ> res(new_vec);
+    return res;
+
+}
+
+template<class Typ>
+Matrix<Typ> Matrix<Typ>::zeros_like(Matrix &r_v) {
+    cout << "ones_like";
+    vector<vector<Typ> > new_vec(r_v.shape.first, vector<Typ>(r_v.shape.second, (Typ) 0));
+    Matrix<Typ> res(new_vec);
+    return res;
+
+}
 
 vector<double> array2vector(double *p,int size )
 {
@@ -317,9 +430,9 @@ int main() {
 
     double v[2] = {1,2,};
     vector<double> vv = array2vector(v,2);
-    Matrix m(vv,2,1);
+    Matrix<double> m(vv, 2, 1);
     m.reshape(2,1);
-    Matrix mt(m.T());
+    Matrix<double> mt(m.T());
     double dot_sum = mt.normal().cross(mt);
     double sum= (m*m).sum();
 
@@ -327,16 +440,17 @@ int main() {
 //    m.T();
 
 
-    Matrix mm(-m);
+    Matrix<double> mm(-m);
 
 
-    Matrix res = mm + mm;
+    Matrix<double> res = mm + mm;
 
 
-
-
-
-
+    Matrix<bool> o = Matrix<bool>::ones(2, 3);
+    int num = 4;
+    bool b = (bool) num;
+    Matrix<double> mo = Matrix<double>::ones_like(m);
+    cout << endl << 233 << "bool" << b << endl;
 
     return 0;
 }
