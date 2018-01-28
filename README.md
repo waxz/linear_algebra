705b56b56ee2df875390b2979ead5f9a8ffa1e4c
waxz <wz67766@163.com>
2018-01-29 01:32:14 +0800

    add note

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # linear_algebra
 c++ linear_algebra
+
+ref:
+http://www.cplusplus.com/reference/valarray/valarray/valarray/
+http://www.linuxtopia.org/online_books/programming_books/c++_practical_programming/c++_practical_programming_202.html
+http://www.cplusplus.com/forum/general/88943/
+https://codereview.stackexchange.com/questions/68486/custom-matrix-class
+https://codereview.stackexchange.com/questions/134391/vector-implementation-in-c
+https://stackoverflow.com/questions/4172722/what-is-the-rule-of-three
+https://codereview.stackexchange.com/questions/154008/custom-stl-vector-in-c
--- main.cpp
+++ main.cpp
@@ -6,12 +6,34 @@
 
 using namespace std;
 
+template<class element_type>
+class matrix {
+public:
+    matrix(size_t width, size_t height) : m_stride(width), m_height(height), m_storage(width * height) {}
+
+    element_type &operator()(size_t row, size_t column) {
+        // column major
+        return m_storage[slice(column, m_height, m_stride)][row];
+
+        // row major
+        return m_storage[slice(row, m_stride, m_height)][column];
+    }
+
+private:
+    std::valarray<element_type> m_storage;
+    size_t m_stride;
+    size_t m_height;
+};
+
 int main() {
+    matrix<double> m(20, 20);
+    m(2, 3) = 5;
 
     valarray<double> vec(10);
     for (size_t i = 0; i < vec.size(); i++)
         vec[i] = 0.3 * i;
     valarray<double> sinc = sin(vec);
+    vec.resize(2);
 
     for (size_t i = 0; i < vec.size(); ++i)
         std::cout << vec[i] << "sin:" << sinc[i] << "sin x" << sin(vec[i]) << endl;
